b1baed6b0416154e95da452cb07f59feed9d697c
shashidharbollu <shashidharbollu77@gmail.com>
2022-04-12 13:33:00 +0530

    [shashidhar] Add : flip coin programme updated

---
+++ BasicProgrammingg/FlipCoin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgrammingg
+{
+    public class FlipCoin
+    {
+        const double A = 0.5;
+        int headCount = 0;
+        int tailCount = 0;
+        public void cointoss()
+        {
+
+            for (int i = 0; i < 50; i++)
+            {
+                Random random = new Random();
+                double num = random.NextDouble();
+                if (num > A)
+                {
+                    headCount++;
+
+                }
+                else
+                {
+                    tailCount++;
+
+                }
+            }
+            double headpercentage = headCount % 100;
+            double tailpercentage = tailCount % 100;
+            Console.WriteLine("the percentage of heads is " + headpercentage);
+            Console.WriteLine("The percentage of tails is " + tailpercentage);
+
+
+        }
+
+        internal void flipCoin()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Flipcoin()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BasicProgrammingg/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BasicProgrammingg;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("welcome to my basic programmes ");
+        bool end = true;
+
+        while (end)
+        {
+            Console.WriteLine("1.flipcoin/n2. end the programme ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                   FlipCoin flipCoin = new FlipCoin();
+                    flipCoin.cointoss();
+                    break;
+                         
+                    }
+        }
+    }
+}
+
