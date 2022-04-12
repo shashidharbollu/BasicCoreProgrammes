5bfe79cc70817644a6530f7a901ff112a3e0fc07
shashidharbollu <shashidharbollu77@gmail.com>
2022-04-12 15:10:12 +0530

    [shashidhar] Add: programme for leap year updated

---
+++ BasicProgrammingg/Leap_Year.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgrammingg
+{
+    public class Leap_Year
+    {
+        public  void LEAPYEAR()
+        {
+            Console.WriteLine("ENTER A YEAR TO CHECK : ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (((year % 4 == 0) && (year % 100 != 0)) ||
+                    (year % 400 == 0))
+            {
+                Console.WriteLine("entered yaer is leap year");
+
+            }
+            else
+            {
+                Console.WriteLine("entered year is not a leap year");
+
+            }
+        }
+
+
+    }
+
+}
+    
--- BasicProgrammingg/Program.cs
+++ BasicProgrammingg/Program.cs
@@ -1,25 +1,28 @@
 ﻿using System;
 using BasicProgrammingg;
-
-public class Program
+class Program
 {
-    public static void Main(String[] args)
+    static void Main(String[] args)
     {
         Console.WriteLine("welcome to my basic programmes ");
         bool end = true;
 
         while (end)
         {
-            Console.WriteLine("1.flipcoin/n2. end the programme ");
+            Console.WriteLine("1.flipcoin/n2. , 2.leap_year/n3. end the programme ");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
-                   FlipCoin flipCoin = new FlipCoin();
+                    FlipCoin flipCoin = new FlipCoin();
                     flipCoin.cointoss();
                     break;
-                         
-                    }
+                case 2:
+                    Leap_Year LEAPYEAR = new Leap_Year();
+                    LEAPYEAR.LEAPYEAR();
+                    break;
+
+            }
         }
     }
 }
